430c75210949fcf736e7c7967b996c5bfc843fee
LuisGerardoLopez <21000377@alumnos.utleon.edu.mx>
2025-06-12 18:08:00 -0600

    PersonajesTerminados 09/06/2025

--- app/src/main/java/com/example/ejemplocomposeridgs903/MainActivity.kt
+++ app/src/main/java/com/example/ejemplocomposeridgs903/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -18,10 +22,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,16 +43,16 @@ data class PersonajeTarjeta(val title: String, val body: String)
 private val tarjetas: List<PersonajeTarjeta> = listOf(
     PersonajeTarjeta("Goku", "El protagonista de la serie, conocido por su gran poder y personalidad amigable."),
     PersonajeTarjeta("Vegeta", "Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters."),
-    PersonajeTarjeta("Piccolo","Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación."),
-    PersonajeTarjeta("Bulma","Bulma es la protagonista femenina de la serie manga Dragon Ball y sus adaptaciones al anime Dragon Ball, Dragon Ball Z, Dragon Ball Super y Dragon Ball GT."),
-    PersonajeTarjeta("Freezer","Freezer es el tirano espacial y el principal antagonista de la saga de Freezer."),
-    PersonajeTarjeta("Gohan","Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT."),
-    PersonajeTarjeta("Krillin","Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball. Es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku."),
-    PersonajeTarjeta("Tenshinhan","Maestro de las artes marciales y miembro de los Z Fighters."),
-    PersonajeTarjeta("Gotenks","Gotenks también conocido inicialmente como Gotrunk en el doblaje al español de España, es el resultado de la Danza de la Fusión llevada a cabo correctamente por Goten y Trunks."),
-    PersonajeTarjeta("Trunks","Hijo de Vegeta y Bulma. Es un mestizo entre humano terrícola y Saiyano nacido en la Tierra, e hijo de Bulma y Vegeta, el cual es introducido en el Arco de los Androides y Cell."),
-    PersonajeTarjeta("Master Roshi","Maestro de artes marciales y mentor de Goku. Conocido bajo el nombre de Muten Rosh, fue en su momento el terrícola más fuerte de la Tierra"),
-    PersonajeTarjeta("Dende","Sucesor de Piccolo como el nuevo Namekian protector de la Tierra. Es un pequeño namekiano, que vivía en el poblado de Moori, junto a su hermano Scargo y otros tantos de su especie.")
+    PersonajeTarjeta("Piccolo", "Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación."),
+    PersonajeTarjeta("Bulma", "Bulma es la protagonista femenina de la serie manga Dragon Ball y sus adaptaciones al anime Dragon Ball, Dragon Ball Z, Dragon Ball Super y Dragon Ball GT."),
+    PersonajeTarjeta("Freezer", "Freezer es el tirano espacial y el principal antagonista de la saga de Freezer."),
+    PersonajeTarjeta("Gohan", "Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT."),
+    PersonajeTarjeta("Krillin", "Amigo cercano de Goku y guerrero valiente, es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku."),
+    PersonajeTarjeta("Tenshinhan", "Maestro de las artes marciales y miembro de los Z Fighters."),
+    PersonajeTarjeta("Gotenks", "Gotenks, también conocido como Gotrunk en España, es el resultado de la Danza de la Fusión llevada a cabo correctamente por Goten y Trunks."),
+    PersonajeTarjeta("Trunks", "Hijo de Vegeta y Bulma. Es un mestizo entre humano terrícola y Saiyano nacido en la Tierra, e hijo de Bulma y Vegeta, el cual es introducido en el Arco de los Androides y Cell."),
+    PersonajeTarjeta("Master Roshi", "Maestro de artes marciales y mentor de Goku. Conocido bajo el nombre de Muten Rosh, fue en su momento el terrícola más fuerte de la Tierra."),
+    PersonajeTarjeta("Dende", "Sucesor de Piccolo como el nuevo Namekian protector de la Tierra. Es un pequeño namekiano que vivía en el poblado de Moori.")
 )
 
 class MainActivity : ComponentActivity() {
@@ -58,57 +68,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Tarjeta(personajes: List<PersonajeTarjeta>){
+fun Tarjeta(personajes: List<PersonajeTarjeta>) {
     LazyColumn {
-        //ImagenHeroe()
-        items(personajes){ personaje ->
+        items(personajes) { personaje ->
             MyPersonajes(personaje)
         }
     }
 }
 
 @Composable
-fun MyPersonajes(personaje: PersonajeTarjeta){
-
-
-    Row {
+fun MyPersonajes(personaje: PersonajeTarjeta) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
         ImagenHeroe()
+        Spacer(modifier = Modifier.width(8.dp))
         Personajes(personaje)
     }
 }
 
 @Composable
-fun Personaje(name: String, color: Color, style: TextStyle){
-    Text(text = name)
+fun Personaje(name: String,color:Color,style: TextStyle,lines:Int=Int.MAX_VALUE) {
+    Text(
+        text = name,
+        color = color,
+        style = style,
+        maxLines = lines
+    )
 }
 
 @Composable
-fun Personajes(personaje: PersonajeTarjeta){
-    Column {
+fun Personajes(personaje: PersonajeTarjeta) {
+
+    var expanded by remember { mutableStateOf(false) }
+
+
+    Column(
+        modifier =Modifier.padding(start =8.dp).clickable{
+        expanded =!expanded
+        }
+    ){
         Personaje(personaje.title,
             MaterialTheme.colorScheme.primary,
             MaterialTheme.typography.headlineMedium)
 
         Personaje(personaje.body,
             MaterialTheme.colorScheme.onBackground,
-            MaterialTheme.typography.bodyLarge)
+            MaterialTheme.typography.bodyLarge,
+            if(expanded) Int.MAX_VALUE else 1)
     }
+
+
+
 }
 
+
 @Composable
-fun ImagenHeroe(){
+fun ImagenHeroe() {
     Image(
-        painter = painterResource(R.drawable.goku_normal),
-        contentDescription = "Goku",
+        painter = painterResource(id = R.drawable.goku_normal),
+        contentDescription = "Imagen del personaje",
         modifier = Modifier
-            .padding(16.dp)
+            .padding(8.dp)
             .size(100.dp)
             .clip(CircleShape)
             .background(MaterialTheme.colorScheme.primary)
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Tarjeta(tarjetas)
